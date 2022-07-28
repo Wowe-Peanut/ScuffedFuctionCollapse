762ea80fc3619a9868632a697f46de0926794353
Ryan <47678647+Wowe-Peanut@users.noreply.github.com>
2022-07-28 00:41:30 -0500

    Propagation Loop (Not working)

--- Bitmaps.py
+++ Bitmaps.py
@@ -1,13 +1,13 @@
 from PIL import Image
 import os
-
-
+import imageio
+import random
 
 
 class Tilemap:
     def __init__(self, tileset_name, size):
 
-        #Read-in all images from tileset
+        #Read-in all images from tileset ----------------------------------------------------------------------------------------------------------------------------------
         self.tile_images = []
         directory = os.path.join(r"C:\Users\Peanu\OneDrive\Desktop\WFC\ScuffedFuctionCollapse\Tilesets", tileset_name)
         for filename in os.listdir(directory):
@@ -18,8 +18,8 @@ class Tilemap:
 
         self.tile_size = self.tile_images[0].size[0]
         self.size = size
-        print(len(self.tile_images))
-        #Create Adjacency Rules
+
+        #Create Adjacency Rules -------------------------------------------------------------------------------------------------------------------------------------------
         self.valid_neighbors = []
         tl = self.tile_size
         edges = [[tl-1, tl//2],[tl//2, 0],[0, tl//2],[tl//2, tl-1]]
@@ -43,26 +43,117 @@ class Tilemap:
             self.valid_neighbors.append(current_valid_neighbors)
                     
 
-        #Initialize Bitmap (Each position will have a superposition: A list of possible positions): A map of tile indexes or possible indexes
-        self.bitmap = [[[i for i in range(len(self.tile_images))] for c in range(size[1])] for r in range(size[0])]
+        #Initialize Bitmap (Each position will have a superposition: A list of possible positions): A map of tile indexes or possible indexes -----------------------------
+        self.bitmap = [[[i for i in range(len(tile_images))] for r in range(size[0])] for c in range(size[1])]
+
+
+        #Variables for keeping track of progress
+        self.total_tiles = self.size[0]*self.size[1]
+        self.tiles_collapsed = 0
+
+        #The bitmaps of each step are logged to be turned into 
+        self.frames = []
         
 
-                               
-    def draw(self, scale=5):
+    #Returns image formed from the bitmap using tile_images as a key-------------------------------------------------------------------------------------------------------               
+    def get_bitmap_image(self, scale=5):
         output = Image.new("RGB", (self.size[0]*self.tile_size, self.size[1]*self.tile_size))
 
         for r in range(len(self.bitmap)):
             for c in range(len(self.bitmap[0])):
-                if type(self.bitmap[r][c]) != int:
+                if len(self.bitmap[r][c]) != 1:
                     continue
                     
-                output.paste(self.tile_images[self.bitmap[r][c]], (r*self.tile_size, c*self.tile_size))
+                output.paste(self.tile_images[self.bitmap[r][c][0]], (r*self.tile_size, c*self.tile_size))
 
-        output.resize((output.size[0]*scale, output.size[1]*scale), Image.Resampling.NEAREST).show()
+        return output.resize((output.size[0]*scale, output.size[1]*scale), Image.Resampling.NEAREST)
 
+
+    #Return (x,y) of tile with the lowest entropy; choose randomly if values are tied -------------------------------------------------------------------------------------
+    def lowest_entropy(self):
+
+        min_entropy = float('inf')
+        min_positions = []
+
+        for r in range(len(self.bitmap)):
+            for c in range(len(self.bitmap[0])):
+                val = self.bitmap[r][c]
+                if len(val) == 1:
+                    continue
+                
+                elif len(val) < min_entropy:
+                    min_entropy = len(val)
+                    min_positions = [(r,c)]
+                    
+                elif len(val) == min_entropy:
+                    min_positions.append((r,c))
+
+        return random.choice(min_positions)
+        
+            
+
+    #Recursively propagates cells with constraints; Only called on a space when its superposition changes/collapses ------------------------------------------------------
+    def propogate(self, r, c):
+        
+        #Removes all tiles in current superposition if they don't have a valid neighbor in each direction
+        current = self.bitmap[r][c]
+        directions = [[r,c+1], [r-1,c], [r, c-1], [r+1, c]]
+        invalid_tiles = []
+        
+        for cur in current:
+            for i, d in enumerate(directions):
+                if 0 <= d[0] < self.size[0] and 0 <= d[1] < self.size[1]:
+                    
+                    #For each valid neighbor of current space, check if it has at least 1 in this direction
+                    valid = False
+                    for valid_neighbor in self.valid_neighbors[cur][i]:
+                        if valid_neighbor in self.bitmap[d[0]][d[1]]:
+                            valid = True
+                            break
+
+                    if not valid:
+                        invalid_tiles.append(cur)
+                        break
+
+        for tile in invalid_tiles:
+            if tile in self.bitmap[r][c]:
+                self.bitmap[r][c].remove(tile)
+
+        #If current superposition updated or is already collapsed:
+        if len(invalid_tiles) > 0 or len(current) == 1:
+
+            #Propogate each surrounding cell if it is not already collapsed
+            for d in directions:
+                if 0 <= d[0] < self.size[0] and 0 <= d[1] < self.size[1]:
+                    if len(self.bitmap[d[0]][d[1]]) > 1:
+                        self.propogate(d[0], d[1])
+                    
+
+    #Creates gif of self.frames and the "imageio" library -----------------------------------------------------------------------------------------------------------------
+    def create_gif(self):
+        pass
+
+
+    #Runs the algorithm until it finishes or until it hits a contradiction ------------------------------------------------------------------------------------------------   
+    def run(self):
+        while self.tiles_collapsed < self.total_tiles:
+            target = self.lowest_entropy()
+
+        
+            #Collapse unit with lowest entropy (or tied for it)
+            self.bitmap[target[0]][target[1]] = [random.choice(self.bitmap[target[0]][target[1]])]
+            self.tiles_collapsed += 1
+    
+            #Reflect changes in surrounding tiles and possibily collapse some
+            self.propogate(target[0], target[1])
+    
+    
+    
+    
 knots = Tilemap("Knots", (13, 13))
-
-knots.draw(5)
+#knots.run()
+knots.get_bitmap_image(8).show()
+print(str(knots.valid_neighbors[0]))
 
 
 
